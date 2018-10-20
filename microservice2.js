174e6efca208c27949691759c9d8991c79272a51
Alexios Karadimos <karadalex@gmail.com>
2018-10-21 00:19:04 +0300

    Get addresses of other microservices from consul

--- microservice2.js
+++ microservice2.js
@@ -2,17 +2,29 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-const consulRegister = 'http://192.168.1.2:8500/v1/catalog/register'
+const consul = 'http://192.168.1.2:8500/v1/catalog'
 const host = '192.168.1.2'
 const port = 3002
 
 app.get('/', (req, res) => res.send('This is microservice2'))
+// Expected routes are /microservice1 /microservice2 /microservice3
+app.get('/:microservice', (req, response) => {
+    axios.get(consul+'/service/'+req.params.microservice)
+        .then((res) => {
+            data = res.data
+            var microHost = data[0]["Address"]
+            var microPort = data[0]["ServicePort"]
+            var microAddr = `http://${microHost}:${microPort}`
+            response.send(`The address of ${req.params.microservice} is <a href="${microAddr}">${microAddr}</a>`)
+        })
+        .catch((error) => console.log(error))
+})
 
 var server = app.listen(port, host, () => {
     console.log(`Microservice 2 is running on port ${port}`)
 
     // Register microservice, when it is fully running
-    axios.put(consulRegister, {
+    axios.put(consul+'/register', {
             "Datacenter": "dc1",
             "Node": "microservice",
             "Address": host,
