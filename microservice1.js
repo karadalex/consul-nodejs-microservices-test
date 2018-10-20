132a98c4ae7a0bea7c6ca9394866d9b3b23bfbe8
Alexios Karadimos <karadalex@gmail.com>
2018-10-20 23:55:33 +0300

    register microservices to consul

--- microservice1.js
+++ microservice1.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
+const consulRegister = 'http://192.168.1.2:8500/v1/catalog/register'
+const host = '192.168.1.2'
 const port = 3001
 
 app.get('/', (req, res) => res.send('This is microservice1'))
 
-app.listen(port, () => console.log(`Microservice 1 is running on port ${port}`))
+var server = app.listen(port, host, () => {
+    console.log(`Microservice 1 is running on port ${port}`)
+
+    // Register microservice, when it is fully running
+    axios.put(consulRegister, {
+            "Datacenter": "dc1",
+            "Node": "microservice",
+            "Address": host,
+            "Service": {
+                "Service": "microservice1",
+                "Port": port
+            }
+        })
+        .then((res) => console.log(res))
+        .catch((error) => console.log(error))
+})
